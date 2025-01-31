14019741f068cb8e13547e585e1368e2a8332c85
layla <layla_adebutu@biswashington.org>
2025-01-31 01:07:31 +0000

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import *
 print("Welcome to the TWICE quiz!!")
 print("Answer questions correctly to get points. Your score will be shown at the end")
-
+score = 0
 
 with open ('questions.txt' , 'r') as file:
     questions = file.readlines()
@@ -10,12 +10,14 @@ random_line_number = randint(0, len(questions) - 1)
 random_question = questions[random_line_number].strip()
 print(f"Question: {random_question}")
 
-
 with open("answers.txt", "r") as file:
     questions = file.readlines()
 
 answer = input()
 if answer+"\n" in questions:
-    print("Correct")
+    print("that's correct!!")
+    score += 1
 else:
-    print("Sorry, wrong answer")
+    print("sorry, wrong answer..")
+
+print(f"your score is now: {score}")
